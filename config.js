111d755c304077e7b0b3cde9cf0aa7013f466dd5
Harshit Rathod <48837539+harshitrathod1@users.noreply.github.com>
2022-12-06 12:24:37 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    prod: {
+      gmailAccount: {
+        user: 'your@email',
+        password: 'your-password',
+        service: 'Gmail'
+      },
+      port: 3600,
+      url: 'http://localhost:3600/'
+    },
+    dev: {
+      gmailAccount: {
+        user: 'your@email',
+        password: 'your-password',
+        service: 'Gmail'
+      },
+      port: 3600,
+      url: 'http://localhost:3600/'
+    }
+  }
